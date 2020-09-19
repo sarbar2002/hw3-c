cfa4acd1a27c21c59c35e6ba8b78a413e76b92a5
sarbar2002 <sxs6666@psu.edu>
2020-09-19 01:43:34 +0000

    int

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include<stdlib.h>
 
 int digit_sum(int n) {
-  if (n/10 != 0){
+  if (n/10 == 0){
     return 0;
   } else{
     int sum = (n % 10) + (digit_sum(n/10));
@@ -15,8 +15,8 @@ int digit_sum(int n) {
 
 
 int main(void) {
-  char*n= readline("Enter an int: ");
-  int number =atoi(n);
-  printf("sum of digits of %d is %d}.\n",number, digit_sum(number));
+  char*number= readline("Enter an int: ");
+  int intnumber =atoi(number);
+  printf("sum of digits of %d is %d}.\n",intnumber, digit_sum(intnumber));
  
 }
